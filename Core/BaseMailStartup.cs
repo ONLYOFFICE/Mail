333cdd42268e56aa2436cad8f9836f967ddf0954
Golikov Dmitrii <dmitriy.golikov@onlyoffice.com>
2023-01-26 13:01:42 +0300

    Refactoring: WebApplication create style changed like Net6

--- Core/BaseMailStartup.cs
+++ Core/BaseMailStartup.cs
@@ -30,86 +30,10 @@ namespace ASC.Mail.Core
 
         public virtual void ConfigureServices(IServiceCollection services)
         {
-            services.AddHttpContextAccessor();
-            //services.AddCustomHealthCheck(Configuration);
-            services.AddScoped<EFLoggerFactory>();
-            services.AddBaseDbContextPool<CoreDbContext>();
-            services.AddBaseDbContextPool<TenantDbContext>();
-            services.AddBaseDbContextPool<UserDbContext>();
-            services.AddBaseDbContextPool<CustomDbContext>();
 
-            services.AddBaseDbContextPool<MailServerDbContext>();
-            services.AddBaseDbContextPool<MailDbContext>();
-
-            services.RegisterFeature();
-            services.AddAutoMapper(GetAutoMapperProfileAssemblies());
-
-            services.AddMemoryCache();
-            //services.AddDistributedCache(Configuration);
-            services.AddDistributedTaskQueue();
-            //services.AddCacheNotify(Configuration);
-            services.AddHttpClient();
-            DIHelper.Configure(services);
-
-            services.AddHttpContextAccessor();
-            services.AddMemoryCache();
-            services.AddHttpClient();
-
-            services.AddScoped<IAccountDao, AccountDao>();
-            services.AddScoped<IAlertDao, AlertDao>();
-            services.AddScoped<IAttachmentDao, AttachmentDao>();
-            services.AddScoped<IChainDao, ChainDao>();
-            services.AddScoped<IContactCardDao, ContactCardDao>();
-            services.AddScoped<IContactDao, ContactDao>();
-            services.AddScoped<IContactInfoDao, ContactInfoDao>();
-            services.AddScoped<ICrmContactDao, CrmContactDao>();
-            services.AddScoped<ICrmLinkDao, CrmLinkDao>();
-            services.AddScoped<IDisplayImagesAddressDao, DisplayImagesAddressDao>();
-            services.AddScoped<IFilterDao, FilterDao>();
-            services.AddScoped<IFolderDao, FolderDao>();
-            services.AddScoped<IImapFlagsDao, ImapFlagsDao>();
-            services.AddScoped<IImapSpecialMailboxDao, ImapSpecialMailboxDao>();
-            services.AddScoped<IMailboxAutoreplyDao, MailboxAutoreplyDao>();
-            services.AddScoped<IMailboxAutoreplyHistoryDao, MailboxAutoreplyHistoryDao>();
-            services.AddScoped<IMailboxDao, ASC.Mail.Core.Dao.MailboxDao>();
-            services.AddScoped<IMailboxDomainDao, MailboxDomainDao>();
-            services.AddScoped<IMailboxProviderDao, MailboxProviderDao>();
-            services.AddScoped<IMailboxServerDao, MailboxServerDao>();
-            services.AddScoped<IMailboxSignatureDao, MailboxSignatureDao>();
-            services.AddScoped<IMailDao, MailDao>();
-            services.AddScoped<IMailGarbageDao, MailGarbageDao>();
-            services.AddScoped<IMailInfoDao, MailInfoDao>();
-            services.AddScoped<IServerAddressDao, ServerAddressDao>();
-            services.AddScoped<IServerDao, ServerDao>();
-            services.AddScoped<IServerDnsDao, ServerDnsDao>();
-            services.AddScoped<IServerDomainDao, ServerDomainDao>();
-            services.AddScoped<IServerGroupDao, ServerGroupDao>();
-            services.AddScoped<ITagAddressDao, TagAddressDao>();
-            services.AddScoped<ITagDao, TagDao>();
-            services.AddScoped<ITagMailDao, TagMailDao>();
-            services.AddScoped<IUserFolderDao, UserFolderDao>();
-            services.AddScoped<IUserFolderTreeDao, UserFolderTreeDao>();
-            services.AddScoped<IUserFolderXMailDao, UserFolderXMailDao>();
-
-
-
-            var diHelper = new DIHelper(services);
-            diHelper.TryAdd<FactoryIndexerMailMail>();
-            diHelper.TryAdd<FactoryIndexerMailContact>();
-            diHelper.TryAdd(typeof(ICacheNotify<>), typeof(KafkaCacheNotify<>));
-
-
-            //var serviceProvider = services.BuildServiceProvider();
-            //var logger = serviceProvider.GetService<ILogger<CrmLinkEngine>>();
-            //services.AddSingleton(typeof(ILogger), logger);
         }
 
-        private IEnumerable<Assembly> GetAutoMapperProfileAssemblies()
-        {
-            return from x in AppDomain.CurrentDomain.GetAssemblies()
-                   where x.GetName().Name!.StartsWith("ASC.")
-                   select x;
-        }
+
 
         public virtual void Configure(IApplicationBuilder app)
         {
--- Services/ASC.Mail.Aggregator.Service/Program.cs
+++ Services/ASC.Mail.Aggregator.Service/Program.cs
@@ -1,12 +1,13 @@
-﻿using ASC.Api.Core.Extensions;
+﻿using ASC.Api.Core.Core;
+using ASC.Api.Core.Extensions;
+using ASC.Common.Logging;
 using ASC.Core.Common.EF;
-using ASC.Mail.Core.Dao.Interfaces;
+using ASC.Core.Common.EF.Context;
 using ASC.Mail.Core.Dao;
-using ASC.Mail.Server.Core.Dao;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Hosting.WindowsServices;
 using ASC.Mail.Core.Dao.Context;
-using ASC.Common;
+using ASC.Mail.Core.Dao.Interfaces;
+using ASC.Mail.Server.Core.Dao;
+using Microsoft.Extensions.Hosting.WindowsServices;
 
 var options = new WebApplicationOptions
 {
@@ -69,23 +70,105 @@ builder.Host.ConfigureAppConfiguration((hostContext, config) =>
         );
 });
 
-builder.Host.ConfigureServices((hostContext, services) =>
-{
-    services.AddSingleton(new ConsoleParser(args));
-    services.AddScoped<AggregatorServiceLauncher>();
-    services.AddScoped<AggregatorServiceScope>();
-    services.AddDistributedTaskQueue();
-    services.AddAutoMapper(Assembly.GetAssembly(typeof(DefaultMappingProfile)));
-    services.AddHostedService<AggregatorServiceLauncher>();
-    services.Configure<HostOptions>(opts => opts.ShutdownTimeout = TimeSpan.FromSeconds(15));
+//builder.Host.ConfigureServices((hostContext, services) =>
+//{
 
-});
+
+//});
 
 //builder.Host.ConfigureNLogLogging();
 
-var startup = new BaseMailStartup(builder.Configuration, builder.Environment);
 
-startup.ConfigureServices(builder.Services);
+builder.Services.AddHttpContextAccessor();
+//services.AddCustomHealthCheck(Configuration);
+builder.Services.AddScoped<EFLoggerFactory>();
+builder.Services.AddBaseDbContextPool<CoreDbContext>();
+builder.Services.AddBaseDbContextPool<TenantDbContext>();
+builder.Services.AddBaseDbContextPool<UserDbContext>();
+builder.Services.AddBaseDbContextPool<CustomDbContext>();
+
+builder.Services.AddBaseDbContextPool<MailServerDbContext>();
+builder.Services.AddBaseDbContextPool<MailDbContext>();
+
+builder.Services.RegisterFeature();
+builder.Services.AddAutoMapper(GetAutoMapperProfileAssemblies());
+
+builder.Services.AddMemoryCache();
+//services.AddDistributedCache(Configuration);
+builder.Services.AddDistributedTaskQueue();
+//services.AddCacheNotify(Configuration);
+builder.Services.AddHttpClient();
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddMemoryCache();
+builder.Services.AddHttpClient();
+
+builder.Services.AddScoped<IAccountDao, AccountDao>();
+builder.Services.AddScoped<IAlertDao, AlertDao>();
+builder.Services.AddScoped<IAttachmentDao, AttachmentDao>();
+builder.Services.AddScoped<IChainDao, ChainDao>();
+builder.Services.AddScoped<IContactCardDao, ContactCardDao>();
+builder.Services.AddScoped<IContactDao, ContactDao>();
+builder.Services.AddScoped<IContactInfoDao, ContactInfoDao>();
+builder.Services.AddScoped<ICrmContactDao, CrmContactDao>();
+builder.Services.AddScoped<ICrmLinkDao, CrmLinkDao>();
+builder.Services.AddScoped<IDisplayImagesAddressDao, DisplayImagesAddressDao>();
+builder.Services.AddScoped<IFilterDao, FilterDao>();
+builder.Services.AddScoped<IFolderDao, FolderDao>();
+builder.Services.AddScoped<IImapFlagsDao, ImapFlagsDao>();
+builder.Services.AddScoped<IImapSpecialMailboxDao, ImapSpecialMailboxDao>();
+builder.Services.AddScoped<IMailboxAutoreplyDao, MailboxAutoreplyDao>();
+builder.Services.AddScoped<IMailboxAutoreplyHistoryDao, MailboxAutoreplyHistoryDao>();
+builder.Services.AddScoped<IMailboxDao, ASC.Mail.Core.Dao.MailboxDao>();
+builder.Services.AddScoped<IMailDaoFactory, MailDaoFactory>();
+builder.Services.AddScoped<IMailboxDomainDao, MailboxDomainDao>();
+builder.Services.AddScoped<IMailboxProviderDao, MailboxProviderDao>();
+builder.Services.AddScoped<IMailboxServerDao, MailboxServerDao>();
+builder.Services.AddScoped<IMailboxSignatureDao, MailboxSignatureDao>();
+builder.Services.AddScoped<IMailDao, MailDao>();
+builder.Services.AddScoped<IMailGarbageDao, MailGarbageDao>();
+builder.Services.AddScoped<IMailInfoDao, MailInfoDao>();
+builder.Services.AddScoped<IServerAddressDao, ServerAddressDao>();
+builder.Services.AddScoped<IServerDao, ServerDao>();
+builder.Services.AddScoped<IServerDnsDao, ServerDnsDao>();
+builder.Services.AddScoped<IServerDomainDao, ServerDomainDao>();
+builder.Services.AddScoped<IServerGroupDao, ServerGroupDao>();
+builder.Services.AddScoped<ITagAddressDao, TagAddressDao>();
+builder.Services.AddScoped<ITagDao, TagDao>();
+builder.Services.AddScoped<ITagMailDao, TagMailDao>();
+builder.Services.AddScoped<IUserFolderDao, UserFolderDao>();
+builder.Services.AddScoped<IUserFolderTreeDao, UserFolderTreeDao>();
+builder.Services.AddScoped<IUserFolderXMailDao, UserFolderXMailDao>();
+
+
+
+var diHelper = new DIHelper(services);
+builder.Services.AddScoped<FactoryIndexerMailMail>();
+builder.Services.AddScoped<FactoryIndexerMailContact>();
+builder.Services.AddScoped(typeof(ICacheNotify<>), typeof(KafkaCacheNotify<>));
+
+builder.Services.AddSingleton(new ConsoleParser(args));
+builder.Services.AddScoped<AggregatorServiceLauncher>();
+
+
+
+builder.Services.AddSingleton<MailQueueItemSettings>();
+builder.Services.AddSingleton<SocketIoNotifier>();
+builder.Services.AddSingleton<MailSettings>();
+builder.Services.AddSingleton<QueueManager>();
+builder.Services.AddSingleton<AggregatorService>();
+builder.Services.AddScoped<AggregatorServiceScope>();
+builder.Services.AddDistributedTaskQueue();
+builder.Services.AddAutoMapper(Assembly.GetAssembly(typeof(DefaultMappingProfile)));
+builder.Services.AddHostedService<AggregatorServiceLauncher>();
+builder.Services.Configure<HostOptions>(opts => opts.ShutdownTimeout = TimeSpan.FromSeconds(15));
+//var serviceProvider = services.BuildServiceProvider();
+//var logger = serviceProvider.GetService<ILogger<CrmLinkEngine>>();
+//services.AddSingleton(typeof(ILogger), logger);
+
+//var startup = new BaseMailStartup(builder.Configuration, builder.Environment);
+
+//startup.ConfigureServices(builder.Services);
 
 builder.Host.ConfigureContainer<ContainerBuilder>((context, builder) =>
 {
@@ -94,6 +177,13 @@ builder.Host.ConfigureContainer<ContainerBuilder>((context, builder) =>
 
 var app = builder.Build();
 
-startup.Configure(app);
+//startup.Configure(app);
 
 await app.RunAsync();
+
+IEnumerable<Assembly> GetAutoMapperProfileAssemblies()
+{
+    return from x in AppDomain.CurrentDomain.GetAssemblies()
+           where x.GetName().Name!.StartsWith("ASC.")
+           select x;
+}
